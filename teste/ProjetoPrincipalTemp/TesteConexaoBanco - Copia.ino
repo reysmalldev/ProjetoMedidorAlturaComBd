0be6e1bc6e9907c82154331b0d10b39ff0c5f319
Cauê de Andrade <caue.andrade.505@gmail.com>
2023-06-15 13:28:46 -0300

    Update TesteConexaoBanco - Copia.ino

--- teste/ProjetoPrincipalTemp/TesteConexaoBanco - Copia.ino
+++ teste/ProjetoPrincipalTemp/TesteConexaoBanco - Copia.ino
@@ -1,30 +1,4 @@
-/*
-  MySQL Connector/Arduino Example : connect by wifi
 
-  This example demonstrates how to connect to a MySQL server from an
-  Arduino using an Arduino-compatible Wifi shield. Note that "compatible"
-  means it must conform to the Ethernet class library or be a derivative
-  with the same classes and methods.
-  
-  For more information and documentation, visit the wiki:
-  https://github.com/ChuckBell/MySQL_Connector_Arduino/wiki.
-
-  INSTRUCTIONS FOR USE
-
-  1) Change the address of the server to the IP address of the MySQL server
-  2) Change the user and password to a valid MySQL user and password
-  3) Change the SSID and pass to match your WiFi network
-  4) Connect a USB cable to your Arduino
-  5) Select the correct board and port
-  6) Compile and upload the sketch to your Arduino
-  7) Once uploaded, open Serial Monitor (use 115200 speed) and observe
-
-  If you do not see messages indicating you have a connection, refer to the
-  manual for troubleshooting tips. The most common issues are the server is
-  not accessible from the network or the user name and password is incorrect.
-
-  Created by: Dr. Charles A. Bell
-*/
 #include <ESP8266WiFi.h>           // Use this for WiFi instead of Ethernet.h
 #include <MySQL_Connection.h>
 #include <MySQL_Cursor.h>
@@ -34,7 +8,7 @@ char user[] = "root";              // MySQL user login username
 char password[] = "";        // MySQL user login password  
 
 // Sample query UPDATE pessoa SET altura = {$altura} WHERE altura = 0"
-char INSERT_SQL[] = "UPDATE db_caue14.pessoa SET = 1.65 WHERE altura = 0";
+char INSERT_SQL[] = "UPDATE dbaltura.pessoa SET = 1.65 WHERE altura = 0";
 
 // WiFi card example
 char ssid[] = "ANDRADE";         // your SSID
