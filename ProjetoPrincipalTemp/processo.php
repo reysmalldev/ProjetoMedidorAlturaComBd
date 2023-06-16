755ec991106ab19b7aba390a15330c7469340440
Cauê de Andrade <110562087+XxCauexX@users.noreply.github.com>
2023-06-16 19:53:20 -0300

    Update processo.php

--- ProjetoPrincipalTemp/processo.php
+++ ProjetoPrincipalTemp/processo.php
@@ -9,30 +9,49 @@
 <body>
     
     <?php
-        include 'conexao.php';
+
+        //include 'conexao.php';
 
         if (filter_input(INPUT_SERVER,'REQUEST_METHOD') == 'POST') {
             $nome = filter_input(INPUT_POST,'nome');
             $idade = filter_input(INPUT_POST,'idade');
             $peso = filter_input(INPUT_POST,'peso');
-            
-            print "nome: {$nome}<br>Idade: {$idade}<br>Peso: {$peso}";
+            print 'Dados que serão inseridos<hr>';
+            print "nome: {$nome}<br>Idade: {$idade}<br>Peso: {$peso}<br>";
         }
         else {
             print "Deu ruim";
         }
 
-        $altura = filter_input(INPUT_GET, 'altura',FILTER_SANITIZE_NUMBER_FLOAT);
-        if (is_null($altura)) {
-            die("Dados inválidos");
-        }
-       
-        $sql = "INSERT INTO pessoa (nome, idade, peso, altura) VALUES ($nome,$idade,$peso,$altura)";
-        if (!$conn->query($sql)) {
+        //Conecta ao banco de dados
+        $servername = "85.10.205.173";
+        $username = "usercaue_14";
+        $password = "mlkzicka";
+        $dbname = "db_caue14";
+
+        //Cria uma conexão
+        $conn = mysqli_connect($servername, $username, $password);
+
+        //Verifica se houve erro na conexão
+        if ($conn->connect_error) {
             //Gravar log de erros
-            die("Erro na gravação dos dados no BD");
+            die("Não foi possível estabelecer conexão com o BD: " . $conn->connect_error);
+        } else {
+            echo "conexão criada com sucesso!";
         }
-       
+
+        //Prepara a query INSERT
+        $sql = "INSERT INTO db_caue14.pessoa (nome, idade, peso) VALUES ('$nome', '$idade', $peso)";
+
+        if (mysqli_query($conn, $sql)) {
+            echo "<br><hr>Inserção realizada com sucesso!";
+                
+        } else {
+            echo "<br><hr>Erro ao inserir na tabela: " . mysqli_error($conn);
+        }
+        
+        //Fecha a conexão
+        mysqli_close($conn);
     ?>
 </body>
 </html>
