8c69be137c3d7a0bbec97aef1e529de6e97f9c21
Guilherme <88351922+reysmalldev@users.noreply.github.com>
2023-06-17 12:49:31 -0300

    Update pessoa.sql

--- ProjetoPrincipalTemp/pessoa.sql
+++ ProjetoPrincipalTemp/pessoa.sql
@@ -1,7 +1,7 @@
 -- Comando para criar tablea pessoa caso precise
 CREATE TABLE pessoa (
-	cod_pessoa INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	cod_pessoa INT PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(35) NOT NULL,
 	idade INT NOT NULL,
 	peso FLOAT NOT NULL,
-	altura FLOAT NULL);
+	altura FLOAT DEFAULT 0);
