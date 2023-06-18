3ece11f1359e93c20df96e67da405647db90a82c
Guilherme <88351922+reysmalldev@users.noreply.github.com>
2023-06-18 18:45:14 -0300

    Update pessoa.sql

--- ProjetoPrincipalTemp/pessoa.sql
+++ ProjetoPrincipalTemp/pessoa.sql
@@ -1,5 +1,5 @@
 -- Comando para criar tabela pessoa caso precise
-CREATE TABLE pessoa (
+CREATE TABLE IF NOT EXISTS pessoa (
 	cod_pessoa INT PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(35) NOT NULL,
 	curso VARCHAR(35) NOT NULL,
